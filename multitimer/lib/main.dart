f0f08766eda383850bc01fe0fdc62d954d6ccdd3
Kilianh07 <hammannkilian@gmail.com>
2023-03-28 10:24:28 +0200

    created splashscreen

--- multitimer/lib/main.dart
+++ multitimer/lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import 'data/Data.dart';
 import 'SplashScreen.dart';
+import 'data/Data.dart';
 
 Data data = new Data();
 SharedPreferences? prefs;
@@ -25,29 +25,53 @@ class MultiTimerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.blueGrey,
+          colorScheme: new ColorScheme(
+              brightness: Brightness.light,
+              primary: Colors.red,
+              onPrimary: Colors.black,
+              secondary: Colors.cyan,
+              onSecondary: Colors.black,
+              error: Colors.red,
+              onError: Colors.black,
+              background: Colors.blueGrey,
+              onBackground: Colors.black,
+              surface: Colors.white,
+              onSurface: Colors.black)),
       darkTheme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          scaffoldBackgroundColor: Colors.white,
+          colorScheme: new ColorScheme(
+              brightness: Brightness.dark,
+              primary: Colors.purple,
+              onPrimary: Colors.black,
+              secondary: Colors.indigo,
+              onSecondary: Colors.black,
+              error: Colors.red,
+              onError: Colors.black,
+              background: Colors.blueGrey,
+              onBackground: Colors.black,
+              surface: Colors.black,
+              onSurface: Colors.white)),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
       home: const SplashScreen(),
