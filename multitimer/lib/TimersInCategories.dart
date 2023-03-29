da346e6ec804145c912dac496f5a020da9725c71
5enseiii <pfanne3@gmx.de>
2023-03-29 11:18:21 +0200

    verbesserungen in TimersInCategories.dart

--- multitimer/lib/TimersInCategories.dart
+++ multitimer/lib/TimersInCategories.dart
@@ -2,6 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
+var ersterAbschnitt = new Text("Erster Abschnitt");
+
 class TimersInCategories extends StatefulWidget {
   const TimersInCategories({Key? key}) : super(key: key);
 
@@ -14,169 +16,225 @@ class _TimersInCategoriesState extends State<TimersInCategories> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(title: new Text(AppLocalizations.of(context)!.cook)),
-      body: new Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Padding(
-            //Nudeln kochen
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              children: [
-                new Container(
-                  width: 290,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Center(
-                      child: new Text(AppLocalizations.of(context)!.cookn)),
-                ),
-                new Container(
-                  width: 1,
-                  height: 145,
-                  color: Colors.black,
-                ),
-                new Container(
-                  width: 100,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.play_arrow),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+      body: Center(
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              //Nudeln kochen
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                children: [
+                  new Container(
+                    width: 290,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          new Text(AppLocalizations.of(context)!.cookn),
+                          new Text("1.)", textScaleFactor: 0.75),
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    width: 1,
+                    height: 145,
+                    color: Colors.black,
+                  ),
+                  new Container(
+                    width: 100,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.play_arrow),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.edit),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.edit),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(CupertinoIcons.trash),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
-                          ),
-                        )
-                      ]),
-                )
-              ],
+                          SizedBox(
+                            width: 100,
+                            height: 49,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(CupertinoIcons.trash),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
+                          )
+                        ]),
+                  )
+                ],
+              ),
             ),
-          ),
-          Padding(
-            //Eier kochen
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              children: [
-                new Container(
-                  width: 290,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Center(
-                      child: new Text(AppLocalizations.of(context)!.cooke)),
-                ),
-                new Container(
-                  width: 1,
-                  height: 145,
-                  color: Colors.black,
-                ),
-                new Container(
-                  width: 100,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.play_arrow),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+            Padding(
+              //Eier kochen
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                children: [
+                  new Container(
+                    width: 290,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          new Text(AppLocalizations.of(context)!.cooke),
+                          new Text("1.)", textScaleFactor: 0.75)
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    width: 1,
+                    height: 145,
+                    color: Colors.black,
+                  ),
+                  new Container(
+                    width: 100,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.play_arrow),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.edit),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.edit),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(CupertinoIcons.trash),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
-                          ),
-                        )
-                      ]),
-                )
-              ],
+                          SizedBox(
+                            width: 100,
+                            height: 49,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(CupertinoIcons.trash),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
+                          )
+                        ]),
+                  )
+                ],
+              ),
             ),
-          ),
-          Padding(
-            //Brötchen aufbacken
-            padding: const EdgeInsets.all(30.0),
-            child: Row(
-              children: [
-                new Container(
-                  width: 290,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Center(
-                      child: new Text(AppLocalizations.of(context)!.cookr)),
-                ),
-                new Container(
-                  width: 1,
-                  height: 145,
-                  color: Colors.black,
-                ),
-                new Container(
-                  width: 100,
-                  height: 145,
-                  color: Colors.yellow,
-                  child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.play_arrow),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+            Padding(
+              //Brötchen aufbacken
+              padding: const EdgeInsets.all(30.0),
+              child: Row(
+                children: [
+                  new Container(
+                    width: 290,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Center(
+                      child: Column(
+                        children: [
+                          new Text(AppLocalizations.of(context)!.cookr),
+                          new Text("1.)", textScaleFactor: 0.75)
+                        ],
+                      ),
+                    ),
+                  ),
+                  new Container(
+                    width: 1,
+                    height: 145,
+                    color: Colors.black,
+                  ),
+                  new Container(
+                    width: 100,
+                    height: 145,
+                    color: Colors.yellow,
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.play_arrow),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(Icons.edit),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
+                          SizedBox(
+                            width: 100,
+                            height: 48,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(Icons.edit),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
                           ),
-                        ),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: Icon(CupertinoIcons.trash),
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(),
-                            padding: EdgeInsets.all(20),
-                          ),
-                        )
-                      ]),
-                )
-              ],
-            ),
-          )
-        ],
+                          SizedBox(
+                            width: 100,
+                            height: 49,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Icon(CupertinoIcons.trash),
+                              style: ElevatedButton.styleFrom(
+                                shape: RoundedRectangleBorder(),
+                                padding: EdgeInsets.all(20),
+                              ),
+                            ),
+                          )
+                        ]),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
