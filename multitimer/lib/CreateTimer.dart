9d7e9a0bf64e0aa9889b4a3d0e57873e99340342
Kilianh07 <hammannkilian@gmail.com>
2023-03-29 16:50:27 +0200

    new timer

--- multitimer/lib/CreateTimer.dart
+++ multitimer/lib/CreateTimer.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:multitimer/data/Data.dart';
+
+import 'NewTimerCreate.dart';
 
 class CreateTimer extends StatefulWidget {
   Data data;
