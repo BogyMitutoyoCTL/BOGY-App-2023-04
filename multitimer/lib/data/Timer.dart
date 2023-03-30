7b90f245d1d0be27edc000d469bf690a5a44dfca
Thomas Weller <thomas.weller@mitutoyo-ctl.de>
2023-03-30 21:53:05 +0200

    prepare time measurements

--- multitimer/lib/data/Timer.dart
+++ multitimer/lib/data/Timer.dart
@@ -1,6 +1,7 @@
 import 'package:json_annotation/json_annotation.dart';
 import 'TimerCategory.dart';
 import 'Section.dart';
+import 'dart:async' as Async;
 
 part 'Timer.g.dart';
 
@@ -20,27 +21,62 @@ class Timer {
   @JsonKey(includeToJson: false, includeFromJson: false)
   late int activesectionnumber;
 
+  Async.Timer? alarm;
+  Stopwatch? stopwatch;
+
+  Section getCurrentSection() {
+    if (activesectionnumber < 0)
+      return Section(duration: Duration.zero, message: "Invalid timer");
+    return sections[activesectionnumber];
+  }
+
   void activate() {
     activesectionnumber = 0;
     isActive = true;
     isExpired = false;
+    alarm = new Async.Timer(getCurrentSection().duration, () {
+      print("Now what`?");
+    });
+    stopwatch = Stopwatch();
+    stopwatch?.start();
   }
 
   void deactivate() {
     isActive = false;
     isExpired = false;
     activesectionnumber = -1;
+    alarmOff();
+  }
+
+  void alarmOff() {
+    alarm?.cancel();
+    alarm = null;
+    stopwatch?.stop();
+    stopwatch = null;
+  }
+
+  Duration getRemainingTime() {
+    if (stopwatch == null) return Duration.zero;
+    var planned = getCurrentSection().duration;
+    var passed = stopwatch!.elapsed;
+    var diff = planned - passed;
+    if (diff < Duration.zero) return Duration.zero;
+    return diff;
   }
 
   void expire() {
     isActive = false;
     isExpired = true;
+    alarmOff();
   }
 
   void nextsection() {
     isActive = true;
     isExpired = false;
     activesectionnumber++;
+    if (activesectionnumber >= sections.length) {
+      deactivate();
+    }
   }
 
   @JsonKey(includeToJson: false, includeFromJson: false)
