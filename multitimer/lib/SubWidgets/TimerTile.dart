9e60dbbc6ff47f97207ee2e0453a27dbe329d1bc
5enseiii <pfanne3@gmx.de>
2023-03-31 09:32:07 +0200

    you can scroll now in the categories

--- multitimer/lib/SubWidgets/TimerTile.dart
+++ multitimer/lib/SubWidgets/TimerTile.dart
@@ -14,80 +14,77 @@ class TimerTile extends StatefulWidget {
 class _TimerTileState extends State<TimerTile> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(30.0),
-        child: Row(
-          children: [
-            Expanded(
-              child: new Container(
-                width: 290,
-                height: 145,
-                color: widget.data.color,
-                child: Center(
-                  child: Column(
-                    children: [
-                      new Text(widget.data.timer.name,
-                          style: Theme.of(context).textTheme.labelMedium),
-                      new Text("",
-                          style: Theme.of(context).textTheme.bodyMedium),
-                    ],
-                  ),
+    return Padding(
+      padding: const EdgeInsets.all(30.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: new Container(
+              width: 290,
+              height: 145,
+              color: widget.data.color,
+              child: Center(
+                child: Column(
+                  children: [
+                    new Text(widget.data.timer.name,
+                        style: Theme.of(context).textTheme.labelMedium),
+                    new Text("", style: Theme.of(context).textTheme.bodyMedium),
+                  ],
                 ),
               ),
             ),
-            new Container(
-              width: 1,
-              height: 145,
-              color: Colors.black,
-            ),
-            new Container(
-              width: 100,
-              height: 145,
-              color: Colors.yellow,
-              child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 100,
-                      height: 48,
-                      child: ElevatedButton(
-                        onPressed: onPlay,
-                        child: Icon(Icons.play_arrow),
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(),
-                          padding: EdgeInsets.all(20),
-                        ),
+          ),
+          new Container(
+            width: 1,
+            height: 145,
+            color: Colors.black,
+          ),
+          new Container(
+            width: 100,
+            height: 145,
+            color: Colors.yellow,
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 100,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: onPlay,
+                      child: Icon(Icons.play_arrow),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(),
+                        padding: EdgeInsets.all(20),
                       ),
                     ),
-                    SizedBox(
-                      width: 100,
-                      height: 48,
-                      child: ElevatedButton(
-                        onPressed: onEdit,
-                        child: Icon(Icons.edit),
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(),
-                          padding: EdgeInsets.all(20),
-                        ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    height: 48,
+                    child: ElevatedButton(
+                      onPressed: onEdit,
+                      child: Icon(Icons.edit),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(),
+                        padding: EdgeInsets.all(20),
                       ),
                     ),
-                    SizedBox(
-                      width: 100,
-                      height: 49,
-                      child: ElevatedButton(
-                        onPressed: onTrash,
-                        child: Icon(CupertinoIcons.trash),
-                        style: ElevatedButton.styleFrom(
-                          shape: RoundedRectangleBorder(),
-                          padding: EdgeInsets.all(20),
-                        ),
+                  ),
+                  SizedBox(
+                    width: 100,
+                    height: 49,
+                    child: ElevatedButton(
+                      onPressed: onTrash,
+                      child: Icon(CupertinoIcons.trash),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(),
+                        padding: EdgeInsets.all(20),
                       ),
-                    )
-                  ]),
-            )
-          ],
-        ),
+                    ),
+                  )
+                ]),
+          )
+        ],
       ),
     );
   }
